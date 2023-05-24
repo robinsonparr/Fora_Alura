75a5e29a7221a2986a5775770931b4078cfead4f
robinsonparr <cerebrito.p@gmail.com>
2023-05-24 17:30:40 -0500

    Creacion de proyecto

---
+++ src/main/java/foro/alura/apiforo/ApiforoApplication.java
@@ -0,0 +1,13 @@
+package foro.alura.apiforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiforoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foro/alura/apiforo/ApiforoApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.alura.apiforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
